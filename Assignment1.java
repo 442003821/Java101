c7e127c212d627bf2a0b0544a497f3835c34ee9d
Muhammed0987 <91156443+Muhammed0987@users.noreply.github.com>
2021-09-26 17:59:14 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,32 @@
+name:محمدعبدالملك الزهراني
+ID:442003821
+
+
+
+import java.util.Scanner;
+
+ class Main {
+    public static void main(String[] args) {
+    Scanner myScanner=new Scanner(System.in);
+      Float age1,age2;
+      System.out.println("Enter the first age");
+      age1 = myScanner.nextFloat();
+      System.out.println("Enter the second age");
+      age2 = myScanner.nextFloat();
+	  System.out.println(age1+age2);
+	 System.out.println(age1-age2);
+	 System.out.println(age1*age2);
+	  System.out.println(age1/age2);
+	   System.out.println(age1%age2);
+	    System.out.println(age1>age2);
+	  System.out.println(age1>=age2);
+	   System.out.println(age1<age2);
+	    System.out.println(age1<=age2);
+	     System.out.println(age1!=age2);
+	      System.out.println(age1=age2);
+	 
+	 
+	 
+	 
+	}
+}
