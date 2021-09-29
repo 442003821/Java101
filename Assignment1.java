a6cf89a6113b5772ef4c9a966b57cd6b78aa9edd
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 23:33:34 +0300

    تاكد من تصحيح العلامات

--- Assignment1.java
+++ Assignment1.java
@@ -1,3 +1,4 @@
+//@ is this a comment or code ????????
 name:محمدعبدالملك الزهراني
 ID:442003821
 
@@ -5,11 +6,12 @@ ID:442003821
 
 import java.util.Scanner;
 //your grade is 2.5/5
-//@ set your name after the import statement
-//@ there is no age but a number !!!!
+//@ place line 6 to be at line 1
  class Main {
     public static void main(String[] args) {
     Scanner myScanner=new Scanner(System.in);
+    //@ change Float to be float in lowercase
+    //@ change age1, age2 to number1 and number to
       Float age1,age2;
       System.out.println("Enter the first age");
       age1 = myScanner.nextFloat();
@@ -25,6 +27,7 @@ import java.util.Scanner;
 	   System.out.println(age1<age2);
 	    System.out.println(age1<=age2);
 	     System.out.println(age1!=age2);
+         //@check the equallity operation
 	      System.out.println(age1=age2);
 	 
 	 
