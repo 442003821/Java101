81c04c5f488900a43ec123b6d8cb4aa7d038862c
محمد الزهراني <m_a_a055@hotmeil.com>
2021-09-30 12:10:22 +0300

    م التعدل

--- Assignment1.java
+++ Assignment1.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 //Your Name:محمد عبدالملك الزهراني
 //Your ID:442003821
  class Assignment1 {
-    public static void Assignment1(String[] args) {
+    public static void main(String[] args) {
     Scanner myScanner=new Scanner(System.in);
     //@ change Float to be float in lowercase
     //@ change age1, age2 to number1 and number to
       float number1,number2;
       System.out.println("Enter the first number");
-      number1 = myScanner.nextfloat();
+      number1 = myScanner.nextFloat();
       System.out.println("Enter the second number");
-      number2 = myScanner.nextfloat();
+      number2 = myScanner.nextFloat();
 	  System.out.println(number1+number2);
 	 System.out.println(number1-number2);
 	 System.out.println(number1*number2);
@@ -18,7 +18,7 @@ import java.util.Scanner;
 	   System.out.println(number1%number2);
 	    System.out.println(number1>number2);
 	  System.out.println(number1>=number2);
-	   System.out.println(anumber1<number2);
+	   System.out.println(number1<number2);
 	    System.out.println(number1<=number2);
 	     System.out.println(number1!=number2);
          //@check the equallity operation
