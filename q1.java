cfd773c4c903f164d5f121448d220418bcdada49
442003821 <91156443+442003821@users.noreply.github.com>
2021-10-12 11:16:44 +0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		
+	
+
+Scanner N=new Scanner(System.in);
+System.out.println("Enter a number");
+ int Run =0;
+int myrun=0;
+Run=N.nextInt();
+Run=myrun/2;
+if(Run*2==myrun){
+System.out.println("Even");}
+    
+
+
+
+else{
+    System.out.println("Odd");}
+}
+}
