f37b9957196c18d9d6797eec9a26fa55ed7da651
442003821 <91156443+442003821@users.noreply.github.com>
2021-11-08 23:24:28 +0300

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //this is a random letter
+        String computerPlays = "rps";
+        Random rnd = new Random();
+        do{
+            System.out.println("If you want to play enter:\n(r or R) for rock,\n(p or P) for paper,\n(s or S) for scissors\nor Enter any key to exit");
+          System.out.println("Enter a char");
+          
+           Scanner N =new Scanner (System.in);
+           String M=N.nextLine();
+           
+
+               M = M.toLowerCase();
+            char AMM= M.charAt(0);
+            System.out.println(AMM);
+            if(AMM != 'r' && AMM != 'p' && AMM != 's'){
+               
+              
+                break;
+            }
+
+
+
+            char computerChoice = computerPlays.charAt(rnd.nextInt(computerPlays.length()));
+            if(computerChoice == AMM){
+           System.out.println("And user tie " + " And computer" + computerChoice);}
+           else if (AMM =='r' && computerChoice == 's' || AMM == 'p' && computerChoice == 'r' || AMM == 's' && computerChoice == 'p'){
+           System.out.println(" You Win ");}
+           else{
+               System.out.println(" Win computer ");}
+           
+           
+           
+            
+
+        System.out.println("\n\n");
+           
+            
+        }while(true);
+    }   
+}
