d28d52c6eb502291205ef4a1b48c3f14869475e8
442003821 <91156443+442003821@users.noreply.github.com>
2021-11-11 11:53:26 +0300

    Create Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main
+{
+	
+	static int add(int L,int O){
+return L+O;
+	 
+	}
+	static int sub(int q ,int a ){
+	   return q-a;
+	    
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	Scanner N = new Scanner(System.in);
+int R = N.nextInt();
+int r = N.nextInt();
+	    int M=N.nextInt();
+	    if(M==1){
+   int result = add(R,r);
+System.out.println(result);}
+
+
+
+else if(M==2){
+    sub(1,2);
+    System.out.println(sub(R,r));
+    
+}
+
+	    
+	    
+	    
+	}
+
+
+	
+	
+	
+}
