0d608d01765f9902fa522ed6d8b7eab86a1501ba
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-01 22:10:40 +0300

    Create even.java

---
+++ even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	int AMM=0;
+	int QYY=0;
+	System.out.println("Enter a namber");
+	Scanner N= new Scanner(System.in);
+	QYY = N.nextInt();
+	AMM = QYY/2;
+	if(AMM*2 == QYY){
+	 System.out.println("Even");}
+	 else{
+	    System.out.println("Odd");}
+	    
+}
+}
