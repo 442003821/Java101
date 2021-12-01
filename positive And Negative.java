307672f59c23d5d17dff3ff6af8ae7321d7ecb8e
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-01 22:57:11 +0300

    Create positive And Negative.java

---
+++ positive And Negative.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner N=new Scanner(System.in);
+	System.out.println("Enter a namber");
+	int var;
+	var=N.nextInt();
+	if(var < 0){
+System.out.println("Negative");}
+else{
+
+System.out.println("Positive");}
+
+	    
+	    
+	    
+	}
+}
