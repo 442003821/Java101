3f4a3589ffc0eafba2a1977d4e2ad4ce44205a82
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-01 23:06:20 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner N=new Scanner(System.in);
+	int age1,age2;
+System.out.println("Enter the first age");
+age1 = N.nextInt();
+System.out.println("Enter the second age");
+age2 = N.nextInt();
+System.out.println(age1>age2);    
+	    
+	}
+}
