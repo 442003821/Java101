2f8980a200915829931a824995e39575874d311f
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-01 22:41:45 +0300

    Create square Width And Height.java

---
+++ square Width And Height.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner N=new Scanner(System.in);
+	System.out.println("Enter a namber");
+	int var;
+	var=N.nextInt();
+	switch(var){
+case 1:
+System.out.println("Enter radiuse");
+float var1=N.nextFloat();
+System.out.println(var*var1*3.14);
+break;
+case 2:
+System.out.println("Enter height");    
+float width=N.nextFloat();
+System.out.println("Enter width");
+float height=N.nextFloat();
+System.out.println(height*width);
+break;
+default:
+System.out.println("Error");
+	    
+	    
+	    
+	    
+	}
+}
+}
