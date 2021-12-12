6bcf74be21ca4114b622db0eae7bbb8405541531
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-12 18:23:17 +0300

    Create read and replace.java

---
+++ read and replace.java
@@ -0,0 +1,12 @@
+public class read and replace
+{
+	
+	
+	public static void main(String[] args) {
+  String name = "muhammed";
+ 
+        System.out.println( name.replace('a', 'X') );
+        
+    }
+ 
+}
