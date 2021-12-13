54f9ae2865bed6127ecd810ba6d335e0752edefe
442003821 <91156443+442003821@users.noreply.github.com>
2021-12-13 20:15:34 +0300

    Create Read word byword.java

---
+++ Read word byword.java
@@ -0,0 +1,13 @@
+public class Read word byword   {
+
+public static void main(String[] args){
+  String name="muhammed";
+  for(String word : name.split(""))
+      System.out.println(word);
+
+
+
+}
+
+
+}
